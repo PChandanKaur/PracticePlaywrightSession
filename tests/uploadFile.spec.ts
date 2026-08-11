82bf63d7686e3b6e68346a1858fe27b4c7f69cda
Preet Kaur <kaurpreetchandan@gmail.com>
2026-08-10 23:38:38 -0400

    Added Upload File

---
+++ tests/uploadFile.spec.ts
@@ -0,0 +1,10 @@
+import{test, expect} from '@playwright/test';
+
+test('Upload The File', async({page}) => {
+
+    await page.goto('https://the-internet.herokuapp.com/upload');
+    await page.getByRole('button', {name:'Choose File'}).setInputFiles('C:\\Users\\Preet\\Downloads\\some-file.txt');
+    await page.getByRole('button',{name:'Upload'}).click();
+
+    await expect(page.getByRole('heading',{name:'File Uploaded!'})).toHaveText('File Uploaded!');
+})
